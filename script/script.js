d16ccca09c47bd5705ea924cfdaf474bf9ceee19
Alena DENIS <basstet067@yahoo.com>
2021-08-19 00:41:11 +0200

    feat(script.js, index.html): timer

--- script/script.js
+++ script/script.js
@@ -1,1 +1,32 @@
+/* eslint-disable strict */
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
 
+  const countTimer = deadline => {
+    const timerHours = document.querySelector('#timer-hours'),
+          timerMinutes = document.querySelector('#timer-minutes'),
+          timerSeconds = document.querySelector('#timer-seconds');
+
+    const getTimeRemaining = () => {
+      const dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60);
+      return { timeRemaining, hours, minutes, seconds };
+    };
+    const updateClock = () => {
+      const timer = getTimeRemaining();
+      if (timer.timeRemaining > 0) {
+        timerHours.textContent = timer.hours < 10 ? `0${timer.hours}` : timer.hours;
+        timerMinutes.textContent = timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes;
+        timerSeconds.textContent = timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds;
+      }
+    };
+    updateClock();
+    setInterval(updateClock, 1000);
+  };
+
+  countTimer('21 august 2021');
+});
